4ec16cd427c1f48fba05ead22aa11f5293bc45da
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-25 21:37:38 +0100

    passing the friend selected down to the splitform UI

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ export default function App() {
         </Button>
       </div>
 
-      {selectedFriend && <FormSplitBill />}
+      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
     </div>
   );
 }
@@ -129,10 +129,10 @@ function FormAddFriend({ onAddFriend }) {
   );
 }
 
-function FormSplitBill() {
+function FormSplitBill({ selectedFriend }) {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with X </h2>
+      <h2>Split a bill with {selectedFriend.name} </h2>
       <label>💰Bill value</label>
       <input type="number" />
       <label>🕴️Your expense</label>
