bb4c29262e85e3f9d538ab771f7bf57ddc3402d9
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-26 00:01:25 +0100

    passing the values and tracking the change

--- src/App.js
+++ src/App.js
@@ -151,13 +151,24 @@ function FormSplitBill({ selectedFriend }) {
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name} </h2>
       <label>💰Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
       <label>🕴️Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) => setPaidByUser(Number(e.target.value))}
+      />
       <label>🧑‍🤝‍🧑{selectedFriend.name}'s expense</label>{" "}
       <input type="text" disabled />
       <label>🤑Who is paying the Bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
